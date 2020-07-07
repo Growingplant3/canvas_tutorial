d3c2ae6684ada49ebd83fc3456f25a39cf9feaeb
下田　佳紀 <y.s@experience.local>
2020-07-07 12:45:59 +0900

    四角形を描画

--- my_canvas.js
+++ my_canvas.js
@@ -3,5 +3,16 @@ function draw(){
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
 
+    //for (var i=0; i<8; i++) {
+      //for (var j=0; j<8; j++) {
+        ctx.beginPath();
+        var x_start = 20// + 60 * i;
+        var y_start = 20// + 60 * j;
+        var x_end = 60
+        var y_end = 60
+        ctx.rect(x_start,y_start,x_end,y_end);
+        ctx.stroke();
+      //}
+    //}
   }
 }
