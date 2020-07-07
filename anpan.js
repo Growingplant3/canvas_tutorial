b548a38d16a338c54bc8c586f21e2166e8916a7e
下田　佳紀 <y.s@experience.local>
2020-07-07 21:24:14 +0900

    アンパン２

--- anpan.js
+++ anpan.js
@@ -4,10 +4,13 @@ function draw(){
     var ctx = canvas.getContext('2d');
 
     ctx.beginPath();
-    ctx.arc(100,100,90,0,Math.PI*2); // 外円
-    ctx.moveTo(50,100);
-    ctx.arc(100,100,30,0,Math.PI*2);
-
+    ctx.arc(100,100,90,0,Math.PI*2,true); // 外円
+    ctx.moveTo(120,100);
+    ctx.arc(100,100,20,0,Math.PI*2,true);
+    ctx.moveTo(60,80);
+    ctx.arc(60,100,20,Math.PI*3/2,Math.PI/2,false);
+    ctx.moveTo(140,80);
+    ctx.arc(140,100,20,Math.PI*3/2,Math.PI/2,true);
     ctx.stroke();
   }
 }
