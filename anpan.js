4c88f050ccd1f7d0061eadb828d87fd30f271607
下田　佳紀 <y.s@experience.local>
2020-07-07 13:09:47 +0900

    アンパンの描画

--- anpan.js
+++ anpan.js
@@ -3,5 +3,11 @@ function draw(){
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
 
+    ctx.beginPath();
+    ctx.arc(100,100,90,0,Math.PI*2); // 外円
+    ctx.moveTo(50,100);
+    ctx.arc(100,100,30,0,Math.PI*2);
+
+    ctx.stroke();
   }
 }
